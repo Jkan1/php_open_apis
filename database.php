579b7640902020708f864704191e0ca4d00522d3
JKan1 <kan.joshi.1@gmail.com>
2020-07-08 22:37:43 +0530

    DB Config Change

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
   global $conn;
-  $conn = new mysqli("localhost", "kankan94_test0", "Qwerty@123", "kankan94_test0");
+  $conn = new mysqli("localhost", "root", "root", "open_api");
   
   if ($conn->connect_error) {
     die("ERROR: Unable to connect: " . $conn->connect_error);
